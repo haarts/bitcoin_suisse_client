e83d2fcc45adf6afdec5570e00715c7dd41b33a3
Harm Aarts <harm@aarts.email>
2019-04-15 10:59:12 +0200

    Add example

--- example/suisse_example.dart
+++ example/suisse_example.dart
@@ -1,6 +1,19 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:suisse/suisse.dart';
 
-main() {
-  var awesome = Awesome();
-  print('awesome: ${awesome.isAwesome}');
+main(List<String> args) async {
+  var client = Client("https://test-paymentterminal.bitcoinsuisse.ch", args[0]);
+  var payment = Payment()
+    ..merchantNumber = "EC66F139"
+    ..terminalNumber = "T53NF5"
+    ..amount = 100
+    ..fromCurrency = "CHF"
+    ..toCurrency = "BTC";
+
+  http.Response response = await client.createPayementRequest(payment);
+  var body = json.decode(response.body);
+
+  response = await client.isChanged(body["Key"]);
+  print(response.body);
 }
